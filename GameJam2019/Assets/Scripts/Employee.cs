a7cdf150994e864f1c29aa5ecb38240d638518fd
jlarussa6 <jlarussa@wbgames.com>
2019-07-22 19:57:02 -0400

    adding people and job data classes

---
+++ GameJam2019/Assets/Scripts/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Employee
+{
+  public int Stealth { get; set; }
+  public int Hacking { get; set; }
+  public int Assasination { get; set; }
+  public string Name { get; set; }
+  public string Description { get; set; }
+}
---
+++ GameJam2019/Assets/Scripts/Job.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Job
+{
+  public int RequiredHacking { get; set; }
+  public int RequiredStealth { get; set; }
+  public int RequiredAssasination { get; set; }
+
+  public int GoldCost { get; set; }
+
+  public int GoldReward { get; set; }
+
+  public int personnelLimit { get; set; }
+
+  public List<Employee> Staff { get; set; }
+}
