f73a9b8bc5df62e754594d3605ba48b4967b453b
MikeLyman <MikeMakesApps@gmail.com>
2019-07-22 21:57:37 -0400

    Adding game scene (for planning -> play -> results UIs) Removing some files from tracking that we don't want to share

---
+++ GameJam2019/Assets/Scripts/GameEvent Scripts/GameEvent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class GameEvent : ScriptableObject
+{
+    private List<GameEventListener> listeners = new List<GameEventListener>();
+
+    public void Raise()
+    {
+        Debug.Log( this.name + " was raised.");
+        for ( int i = listeners.Count - 1; i >= 0; i-- )
+        {
+            listeners[ i ].OnEventRaised();
+        }
+    }
+
+    public void RegisterListener( GameEventListener listener )
+    {
+        listeners.Add( listener );
+    }
+
+    public void UnregisterListener( GameEventListener listener )
+    {
+        listeners.Add( listener );
+    }
+}
