07009aee1070ad405b36fb1bf4a9569b4bae954a
james.larussa@gmail.com <james.larussa@gmail.com>
2019-07-23 13:06:44 -0400

    Adding a draft of the drop script to jobs

---
+++ GameJam2019/Assets/Scripts/JobDropArea.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JobDropArea : MonoBehaviour 
+{
+    [SerializeField]
+    private BoxCollider2D jobCollider = null;
+
+    [SerializeField]
+    private Job targetJob = null;
+
+    private Collider pendingCollider = null;
+
+    private void OnTriggerEnter( Collider other )
+    {
+        pendingCollider = other;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        pendingCollider = null;
+    }
+
+    private void OnMouseUp()
+    {
+        if (pendingCollider != null)
+        {
+            var Employee = pendingCollider?.gameObject?.GetComponent<EmployeeView>()?.EmployeeData;
+            targetJob.AddEmployee(Employee);
+        }
+    }
+
+}
