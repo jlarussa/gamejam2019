d4107482d2d45d4f7af76fb2107f74caa2b1e432
MikeLyman <MikeMakesApps@gmail.com>
2019-07-24 19:01:44 -0400

    Adding basic dragging to employees

---
+++ GameJam2019/Assets/Scripts/EmployeeDragger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EmployeeDragger : EventTrigger {
+
+private bool dragging;
+	
+	// Update is called once per frame
+	void Update () {
+		if ( dragging )
+		{
+			transform.position = new Vector2( Input.mousePosition.x, Input.mousePosition.y );
+		}
+	}
+
+	public override void OnPointerDown( PointerEventData eventData)
+	{
+		dragging = true;
+	}
+
+	public override void OnPointerUp( PointerEventData eventData )
+	{
+		dragging = false;
+	}
+
+}
