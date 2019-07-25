392f5365ac680ea62c2fa4ad4b2ed58cf12bd34b
MikeLyman <MikeMakesApps@gmail.com>
2019-07-24 20:27:32 -0400

    Making portraits work for employee cards. Adding scriptable object for portraits.

--- GameJam2019/Assets/Scripts/Employee.cs
+++ GameJam2019/Assets/Scripts/Employee.cs
@@ -11,4 +11,5 @@ public class Employee
   public int Assassination { get; set; }
   public string Name { get; set; }
   public string Description { get; set; }
+  public int PortraitIndex {get; set;}
 }
---
+++ GameJam2019/Assets/Scripts/ImageList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ImageList")]
+public class ImageList : ScriptableObject
+{
+  [SerializeField]
+  private List<Sprite> images;
+  public List<Sprite> Images => images; 
+}
