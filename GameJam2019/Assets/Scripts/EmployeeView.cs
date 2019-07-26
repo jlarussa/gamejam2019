ae1a5c74f8d6cb2cfea99b13f080869f3d2bfcf4
MikeLyman <MikeMakesApps@gmail.com>
2019-07-26 12:19:01 -0400

    Adding slivers to 0 values of employee bars. Also adding tinting to character portraits based on highest skill

--- GameJam2019/Assets/Scripts/EmployeeView.cs
+++ GameJam2019/Assets/Scripts/EmployeeView.cs
@@ -6,6 +6,10 @@ using UnityEngine.UI;
 public class EmployeeView : MonoBehaviour
 {
     public Image PortraitUI;
+    public Color PortraitColorHacking;
+    public Color PortraitColorAssassination;
+    public Color PortraitColorStealth;
+
     public Text NameText;
     public Text DescriptionText;
 
@@ -56,6 +60,7 @@ public class EmployeeView : MonoBehaviour
         } 
     }
 
+    private float barSliverAmt = 0.1f;
     private void UpdateStatViews()
     {
         HackingText.text = string.Empty;
@@ -63,20 +68,47 @@ public class EmployeeView : MonoBehaviour
         {
             HackingText.text = EmployeeData.Hacking.ToString();
         }
-        HackingSlider.value = EmployeeData.Hacking;
+        HackingSlider.value = Mathf.Max( barSliverAmt, EmployeeData.Hacking );
 
         AssassinationText.text = string.Empty;
         if ( EmployeeData.Assassination > 0 )
         {
             AssassinationText.text = EmployeeData.Assassination.ToString();
         }
-        AssassinationSlider.value = EmployeeData.Assassination;
+        AssassinationSlider.value = Mathf.Max( barSliverAmt, EmployeeData.Assassination);
 
         StealthText.text = string.Empty;
         if ( EmployeeData.Stealth > 0 )
         {
             StealthText.text = EmployeeData.Stealth.ToString();
         }
-        StealthSlider.value = EmployeeData.Stealth;
+        StealthSlider.value = Mathf.Max( barSliverAmt, EmployeeData.Stealth );
+        TintPortait();
+    }
+
+    private void TintPortait()
+    {
+        // 0 is hacking, 1 is assassination, 2 is stealth
+        int highestStat = 0;
+        if ( EmployeeData.Assassination > EmployeeData.Hacking )
+        {
+            highestStat = 1;
+        }
+        if ( EmployeeData.Stealth > EmployeeData.Hacking && EmployeeData.Stealth > EmployeeData.Assassination )
+        {
+            highestStat = 2;
+        }
+        switch ( highestStat )
+        {
+            case 0:
+                PortraitUI.color = PortraitColorHacking;
+                break;
+            case 1:
+                PortraitUI.color = PortraitColorAssassination;
+                break;
+            case 2:
+                PortraitUI.color = PortraitColorStealth;
+                break;
+        }
     }
 }
