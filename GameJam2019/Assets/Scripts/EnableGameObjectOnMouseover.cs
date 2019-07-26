c600fc099b58b06e74b18d2f0c7605c56b361dc1
james.larussa@gmail.com <james.larussa@gmail.com>
2019-07-26 12:57:13 -0400

    Added the text bubbles

---
+++ GameJam2019/Assets/Scripts/EnableGameObjectOnMouseover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EnableGameObjectOnMouseover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    [SerializeField]
+    GameObject objectToEnable;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        objectToEnable.SetActive(true);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        objectToEnable.SetActive(false);
+    }
+}
