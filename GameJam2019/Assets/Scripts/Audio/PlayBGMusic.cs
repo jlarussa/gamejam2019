b275dff10d02b62c7f4e3591177ffd4549f89008
kshuffleton <kshuffleton@gmail.com>
2019-07-27 16:23:11 -0400

    added music, game over and time running out audio and a time running out screen overlay

---
+++ GameJam2019/Assets/Scripts/Audio/PlayBGMusic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayBGMusic : MonoBehaviour {
+
+    protected AudioSource audioSource;
+    public AudioClip audioClip;
+
+    private void Start()
+    {
+        if(audioSource == null)
+        {
+            audioSource = GameObject.FindWithTag("BGMusicSource").GetComponent<AudioSource>();
+        }
+    }
+
+    public void playClip()
+    {
+        audioSource.clip = audioClip;
+        audioSource.Play();
+    }
+}
